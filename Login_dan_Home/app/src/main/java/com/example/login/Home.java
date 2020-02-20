9ab881c58d719929c5a9cf2c6405a7db82d864a1
yose kornelius <yose.kornelius@ti.ukdw.ac.id>
2020-02-21 01:56:12 +0700

    Login, Toast,dan Home
    sudah berhasil membuat tampilan home ditengah, ubah background, input field kotak, tulisan sign up clickable, dapat mengeluarkan toast berupa jika input field kosong maka keluar input field salah, toast email invalid, dan isi email dan password dapat di toast, untuk landing ke home masih error (force close), akan segera diperbaiki

---
+++ Login_dan_Home/app/src/main/java/com/example/login/Home.java
@@ -0,0 +1,47 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Home extends AppCompatActivity {
+
+    String text;
+
+    Button button1;
+    TextView textView2;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = editText.getText().toString();
+                if (TextUtils.isEmpty(text)){
+                    Toast.makeText(Home.this,
+                            "input kosong", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    textView2.setText(text);
+
+                }
+            }
+        });
+
+    }
+
+    private void showToast(String text) {
+
+        Toast.makeText(Home.this, text, Toast.LENGTH_SHORT).show();
+    }
+}
